911003ce820bfd7d5807ccf4e090d62695028eca
Newton Bujiku <bujikun@vodacom.co.tz>
2023-07-07 19:19:59 +0300

    Change IP for cross origin

--- src/main/java/tz/co/vodacom/bujikun/cicd/cicddemo/controller/UserController.java
+++ src/main/java/tz/co/vodacom/bujikun/cicd/cicddemo/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = {"http://localhost"})
+@CrossOrigin(origins = {"http://127.0.0.1"})
 public class UserController {
     private static final List<User> users = Arrays.asList(
             new User[]{new User("John", "Doe", 45),
